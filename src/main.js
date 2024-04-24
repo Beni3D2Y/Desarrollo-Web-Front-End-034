5fbbb37f4d5437b76fb7a441e71a8bc8e07a6654
Beni3D2Y <estudillobenito590@gmail.com>
2024-04-23 23:56:35 -0600

    Variables y constantes

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let name ="Benito";
+name = "Abraham";
+
+const lastname = "Estudillo";
+
+const condition = true;
+
+if(condition){
+    const lastname ="Hyper";
+    console.log(lastname);
+}
+console.log("Hola mundo " + name + ' ' + lastname);
